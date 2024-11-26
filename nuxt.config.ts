f636bb622c3738bb27044006ed8cac40d2aee4f7
veryCrunchy <me@verycrunchy.dev>
2024-11-26 20:39:42 +0100

    wip: homepage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@unocss/nuxt']
-})
+  css: [
+    "@unocss/reset/normalize.css",
+    "@unocss/reset/sanitize/sanitize.css",
+    "@unocss/reset/sanitize/assets.css",
+    "@unocss/reset/eric-meyer.css",
+  ],
+  modules: ["@unocss/nuxt"],
+});
